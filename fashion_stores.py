860160b5c2f63ddd4821507f28b265353fe6e0a5
Asmaa <asmaaelhawary333@gmail.com>
2026-03-28 20:00:39 +0200

    Update fashion_stores.py

--- fashion_stores.py
+++ fashion_stores.py
@@ -6,10 +6,12 @@ item1=input("what do you want from our store \n").lower()
 if item1 in availabe_clothes:
     shopping_cart.append(item1)
     print("added successfuly")
-item2=input("do you want another item ?\n").lower()
+item2=input("do you want another item or( enter to skip) ?\n").lower()
 if item2 in availabe_clothes:
     shopping_cart.append(item2)
     print("added successfuly")
+elif item2=="":
+    print("skipped")
 else:
     print("sorry, we do not have this item")
     
